dee79f9b8a55f1954ed17875383ad2d07933e1e9
a-karpov-dev <karpov@smuit.ru>
2026-03-15 18:55:14 +0400

    [fix] import

--- custom_components/http_relay_16/__init__.py
+++ custom_components/http_relay_16/__init__.py
@@ -2,13 +2,14 @@ import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN, CONF_HOST, CONF_PORT, CONF_SCAN_INTERVAL, CONF_RELAY_COUNT
-from .switch import RelayCoordinator 
 
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[str] = ["switch", "sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    from .switch import RelayCoordinator 
+
     host = entry.options.get(CONF_HOST, entry.data.get(CONF_HOST))
     port = entry.options.get(CONF_PORT, entry.data.get(CONF_PORT))
     scan_interval = entry.options.get(CONF_SCAN_INTERVAL, entry.data.get(CONF_SCAN_INTERVAL, 30))
@@ -22,7 +23,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     entry.async_on_unload(entry.add_update_listener(update_listener))
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
+    
     return True
 
 async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
